411ef24a5aa552e0f8ed55af818edd251a49f298
azamat-dev043 <azamat_duisenbekov@icloud.com>
2025-11-13 16:53:12 +0500

    initial commit

---
+++ src/main/java/com/example/dreamauto/DreamautoApplication.java
@@ -0,0 +1,13 @@
+package com.example.dreamauto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamautoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamautoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dreamauto/DreamautoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dreamauto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamautoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
